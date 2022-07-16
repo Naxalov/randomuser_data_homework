a8a2a09952d1185f1e1da73ee3cbfe8d728b8648
ZARIFMAC <uzbzarif@gmail.com>
2022-07-16 10:55:56 +0500

    count

--- get_count_users.py
+++ get_count_users.py
@@ -1,6 +1,5 @@
-from webbrowser import get
-import get_data
 
+from get_data import get_data
 def get_count_users(data:dict) -> int:
     """
     You are given dictionary. Find the number of users.
@@ -10,4 +9,11 @@ def get_count_users(data:dict) -> int:
     Returns:
         int: number of users
     """
-    
+    results = data['results']
+    k = 0
+    for i in results:
+        k+=1
+    return k
+
+data = get_data('randomuser_data.json')
+print(get_count_users(data))
